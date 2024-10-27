55655fc2ef2ffa27bcfad96d4000f10c155230f3
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-26 21:15:36 -0700

    Entry Point

--- CLIENT/src/main.jsx
+++ CLIENT/src/main.jsx
@@ -1,10 +1,15 @@
+import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+import App from "./App";
+import { RestaurantContextProvider } from "./context/RestaurantContexts";
+
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+root.render(
   <StrictMode>
-    <App />
+    <RestaurantContextProvider>
+      <App />
+    </RestaurantContextProvider>
   </StrictMode>
 );

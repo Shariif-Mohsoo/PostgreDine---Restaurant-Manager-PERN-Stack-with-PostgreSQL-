c61d9ae2d9256ca293dbf931be8fe0a5c0a7919d
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-28 07:47:27 -0700

    Detail Page For Restaurant

--- CLIENT/src/routes/RestaurantDetailPage.jsx
+++ CLIENT/src/routes/RestaurantDetailPage.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantContexts";
+import RestaurantFinder from "../Apis/RestaurantFinder";
 
 const RestaurantDetailPage = () => {
-  return <div>RestaurantDetailPage</div>;
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } =
+    useContext(RestaurantsContext);
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await RestaurantFinder.get(`/${id}`);
+        // console.log(response.data.data.restaurant);
+        setSelectedRestaurant(response.data.data.restaurant);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
 };
 
 export default RestaurantDetailPage;

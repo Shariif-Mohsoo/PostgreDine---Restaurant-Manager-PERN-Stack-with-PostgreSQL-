3eefae5a1812d4bb790936987aa8a1e9b6785ece
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-29 22:01:21 -0700

    Update Restaurant Page

--- CLIENT/src/routes/UpdatePage.jsx
+++ CLIENT/src/routes/UpdatePage.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-
+import UpdateRestaurants from "../components/UpdateRestaurants";
 const UpdatePage = () => {
-  return <div>UpdatePage</div>;
+  return (
+    <div>
+      <h1 className="text-center">Update Restaurants</h1>
+      <UpdateRestaurants />
+    </div>
+  );
 };
 
 export default UpdatePage;

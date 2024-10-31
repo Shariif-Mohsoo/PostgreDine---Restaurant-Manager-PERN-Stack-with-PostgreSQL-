69071476fb1133167c163db4ca78c123f7348b87
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-30 22:42:20 -0700

    Restaurants Detail Page

--- CLIENT/src/routes/RestaurantDetailPage.jsx
+++ CLIENT/src/routes/RestaurantDetailPage.jsx
@@ -2,6 +2,9 @@ import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantContexts";
 import RestaurantFinder from "../Apis/RestaurantFinder";
+// import StarRating from "../components/StarRating";
+import Reviews from "../components/Reviews";
+import AddReview from "../components/AddReview";
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
@@ -12,14 +15,29 @@ const RestaurantDetailPage = () => {
       const fetchData = async () => {
         const response = await RestaurantFinder.get(`/${id}`);
         // console.log(response.data.data.restaurant);
-        setSelectedRestaurant(response.data.data.restaurant);
+        // console.log(response);
+        setSelectedRestaurant(response.data.data);
       };
       fetchData();
     } catch (error) {
       console.log(error.message);
     }
   }, []);
-  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
+  return (
+    <div>
+      {selectedRestaurant && (
+        <>
+          <h1 className="text-center display-1">
+            {selectedRestaurant.restaurant.name}
+          </h1>
+          <div className="mt-3">
+            <Reviews reviews={selectedRestaurant.review} />
+          </div>
+          <AddReview />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default RestaurantDetailPage;
